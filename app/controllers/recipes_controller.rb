e4d9fef9d3cf4f9911d5893fe16a60ef081d316c
Otto <ottoburney@gmail.com>
2020-06-22 15:23:56 +0100

    added http authentication for CRUD on recipes (commented out for now)- will put real details in env file later

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @recipes = Recipe.all
   end
