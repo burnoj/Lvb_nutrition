6af163f0656a3ca7b4c1183c9b5ccaf408a76eda
Otto <ottoburney@gmail.com>
2020-06-22 16:36:11 +0100

    done validations for models

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,10 +1,16 @@
 class RecipeIngredientsController < ApplicationController
+
   def new
     @recipe = Recipe.find(params[:recipe_id])
-    @ingredient = RecipeIngredient.new
+    @recipe_ingredient = RecipeIngredient.new
   end
 
   def create
+    @recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_ingredient.recipe = @recipe
+    @recipe_ingredient.save
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
@@ -16,4 +22,9 @@ class RecipeIngredientsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def recipe_ingredient_params
+    params.require(:recipe_ingredient).permit(:amount)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,11 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients, dependent: :destroy
+  has many :ingredients, through: :recipe_ingredients
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :description, presence: true
+  validates :instructions, presence: true
+  validates :dietary_info, presence: true
+  validates :dcooking_time, presence: true
 end
