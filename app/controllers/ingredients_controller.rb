7cc19c8fc11c0834bc32fd2e3b972c3e5cb88545
Otto <ottoburney@gmail.com>
2020-06-23 12:58:24 +0100

    adding simple form for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,28 @@
 class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:id])
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      render :new, notice: "ingredient added"
+    else
+      render :new, alert: "ingredient invalid or already exists"
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+
+    redirect_to recipes_path
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'user_recipes', to: 'user_recipes#index'
   get 'user_recipes/:id', to: 'user_recipes#show', as: "user_recipe"
 
+  resources :ingredients, only: [:new]
+
   resources :recipes do
     resources :recipe_ingredients, only: [:new, :create]
   end
