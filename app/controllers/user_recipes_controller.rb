7b82c8195895a9df3f22a6ec24f4590191dc2378
Otto <ottoburney@gmail.com>
2020-06-23 09:57:47 +0100

    added logic for recipe of the day, changing every 24 hours

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,7 +1,9 @@
 class UserRecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    @recipe_of_the_day = Recipe.all.sample
+    config.cache_store = :memory_store
+    recipe_of_the_day = Rails.cache.fetch("recipe", expires_in: 1.day) { Recipe.all.sample(1) }
+    @recipe_of_the_day = recipe_of_the_day[0]
   end
 
   def show
