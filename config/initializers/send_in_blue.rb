d92356ff2fa64a98ed201a92e86ce52aaaf19a79
Otto <ottoburney@gmail.com>
2020-06-24 12:52:31 +0100

    app sends test email to yahoo account every time the server is started

--- config/initializers/send_in_blue.rb
+++ config/initializers/send_in_blue.rb
@@ -9,13 +9,14 @@ end
 
 api_instance = SibApiV3Sdk::SMTPApi.new
 
-template_id = 1 # Integer | id of the template
+template_id = 1 # Integer | Id of the template
+
+send_test_email = SibApiV3Sdk::SendTestEmail.new # SendTestEmail |
 
 
 begin
-  #Returns the template information
-  result = api_instance.get_smtp_template(template_id)
-  p result
+  #Send a template to your test list
+  api_instance.send_test_template(template_id, send_test_email)
 rescue SibApiV3Sdk::ApiError => e
-  puts "Exception when calling SMTPApi->get_smtp_template: #{e}"
+  puts "Exception when calling SMTPApi->send_test_template: #{e}"
 end
