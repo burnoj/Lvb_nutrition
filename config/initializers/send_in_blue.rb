64ec2aacb0dff17b4b553297452d0c1d7c5aae04
Otto <ottoburney@gmail.com>
2020-06-24 13:11:33 +0100

    commented out sendinblue initializer

--- config/initializers/send_in_blue.rb
+++ config/initializers/send_in_blue.rb
@@ -1,22 +1,22 @@
 # Load the gem
-require 'sib-api-v3-sdk'
+# require 'sib-api-v3-sdk'
 
-# Setup authorization
-SibApiV3Sdk.configure do |config|
-  # Configure API key authorization: api-key
-  config.api_key['api-key'] = ENV['SIB_API_KEY']
-end
+# # Setup authorization
+# SibApiV3Sdk.configure do |config|
+#   # Configure API key authorization: api-key
+#   config.api_key['api-key'] = ENV['SIB_API_KEY']
+# end
 
-api_instance = SibApiV3Sdk::SMTPApi.new
+# api_instance = SibApiV3Sdk::SMTPApi.new
 
-template_id = 1 # Integer | Id of the template
+# template_id = 1 # Integer | Id of the template
 
-send_test_email = SibApiV3Sdk::SendTestEmail.new # SendTestEmail |
+# send_test_email = SibApiV3Sdk::SendTestEmail.new # SendTestEmail |
 
 
-begin
-  #Send a template to your test list
-  api_instance.send_test_template(template_id, send_test_email)
-rescue SibApiV3Sdk::ApiError => e
-  puts "Exception when calling SMTPApi->send_test_template: #{e}"
-end
+# begin
+#   #Send a template to your test list
+#   api_instance.send_test_template(template_id, send_test_email)
+# rescue SibApiV3Sdk::ApiError => e
+#   puts "Exception when calling SMTPApi->send_test_template: #{e}"
+# end
