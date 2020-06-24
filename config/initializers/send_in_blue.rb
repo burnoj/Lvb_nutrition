1b97d2d21f7a01d97419625f756ba6d1792fead4
Otto <ottoburney@gmail.com>
2020-06-24 11:14:47 +0100

    successfully sent test email to yahoo address

--- config/initializers/send_in_blue.rb
+++ config/initializers/send_in_blue.rb
@@ -1,28 +1,28 @@
-# Load the gem
+# load the gem
 require 'sib-api-v3-sdk'
-
-# Setup authorization
+# setup authorization
 SibApiV3Sdk.configure do |config|
   # Configure API key authorization: api-key
   config.api_key['api-key'] = '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'
   # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
-  config.api_key_prefix['api-key'] = ENV['otto-api-key']
+  #config.api_key_prefix['api-key'] = 'Bearer'
 
   # Configure API key authorization: partner-key
-  config.api_key['partner-key'] = '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'
+  config.api_key['partner-key'] = 'Yxkeysib-1e0e77dfd32a96dd7e64a011640a0676274ff70a08c4c694a73245e92e1600c4-PgKApcvQzqE0LMFH'
   # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
-  config.api_key_prefix['partner-key'] = ENV['otto-api-partner-key']
+  #config.api_key_prefix['partner-key'] = 'Bearer'
 end
 
 api_instance = SibApiV3Sdk::SMTPApi.new
 
-template_id = 1 # Integer | id of the template
+template_id = 1 # Integer | Id of the template
+
+send_test_email = SibApiV3Sdk::SendTestEmail.new # SendTestEmail |
 
 
 begin
-  #Returns the template information
-  result = api_instance.get_smtp_template(template_id)
-  p result
+  #Send a template to your test list
+  api_instance.send_test_template(template_id, send_test_email)
 rescue SibApiV3Sdk::ApiError => e
-  puts "Exception when calling SMTPApi->get_smtp_template: #{e}"
+  puts "Exception when calling SMTPApi->send_test_template: #{e}"
 end
