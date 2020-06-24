4a02438b5e7fad59933243293dd47617804641da
Otto <ottoburney@gmail.com>
2020-06-24 14:21:33 +0100

    changed model and controller to subscribers, added new and create methods in subscriber controller

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,20 @@
+class SubscribersController < ApplicationController
+  def new
+    @subscriber = Subscriber.new
+  end
+
+  def create
+    @subscriber = subscriber.find(subscriber_params)
+    if @subscriber.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_120608) do
+ActiveRecord::Schema.define(version: 2020_06_24_131342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_06_24_120608) do
     t.string "dietary_info"
     t.integer "cooking_time"
     t.integer "serves"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
