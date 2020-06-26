c4555fa6f6897967219c131da7fc32b0dd9bda20
Otto <ottoburney@gmail.com>
2020-06-26 12:34:37 +0100

    added automatic contact population to sendinblue via Subscriber model

--- config/initializers/send_in_blue.rb
+++ config/initializers/send_in_blue.rb
@@ -1,22 +1,41 @@
-# Load the gem
-# require 'sib-api-v3-sdk'
+# Load the Gem
+require 'sib-api-v3-sdk'
+require 'uri'
+require 'net/http'
+require 'openssl'
 
-# # Setup authorization
-# SibApiV3Sdk.configure do |config|
-#   # Configure API key authorization: api-key
-#   config.api_key['api-key'] = ENV['SIB_API_KEY']
-# end
+# Setup authorization
+SibApiV3Sdk.configure do |config|
+  # Configure API key authorization: api-key
+  config.api_key['api-key'] = ENV['SIB_API_KEY']
+end
 
-# api_instance = SibApiV3Sdk::SMTPApi.new
-
+api_instance = SibApiV3Sdk::SMTPApi.new
 # template_id = 1 # Integer | Id of the template
-
 # send_test_email = SibApiV3Sdk::SendTestEmail.new # SendTestEmail |
 
+begin
+  # api_instance.send_test_template(template_id, send_test_email)
+rescue SibApiV3Sdk::ApiError => e
+  puts "Exception when calling SMTPApi->send_test_template: #{e}"
+end
 
-# begin
-#   #Send a template to your test list
-#   api_instance.send_test_template(template_id, send_test_email)
-# rescue SibApiV3Sdk::ApiError => e
-#   puts "Exception when calling SMTPApi->send_test_template: #{e}"
-# end
+# ------------ Contact import ------------ #
+url = URI("https://api.sendinblue.com/v3/contacts")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+subs = Subscriber.all.to_a
+subscribers = subs.map {|s| s.email}
+
+subscribers.each do |s|
+  request = Net::HTTP::Post.new(url)
+  request["accept"] = 'application/json'
+  request["content-type"] = 'application/json'
+  request["api-key"] = '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'
+  request.body = "{\"updateEnabled\":false,\"email\":\"#{s}\"}"
+  response = http.request(request)
+  puts response.read_body
+end
