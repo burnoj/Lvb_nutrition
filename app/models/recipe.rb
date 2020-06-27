607e6a3565aacde5d18faa528bfb5596f779494a
Otto <ottoburney@gmail.com>
2020-06-27 17:12:05 +0100

    new recipe form now takes dietary info as booleans (4 separate)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,9 +6,5 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
   validates :description, presence: true
   validates :instructions, presence: true
-  validates :vegan, presence: true
-  validates :vegetarian, presence: true
-  validates :dairy_free, presence: true
-  validates :gluten_free, presence: true
   validates :cooking_time, presence: true
 end
