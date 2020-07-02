43caf31342445075ead7f00f9d71f79986be496b
Otto <ottoburney@gmail.com>
2020-07-02 16:11:21 +0100

    changed dev and production environments for contact email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -61,14 +58,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors= true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-    address:              'smtp.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            "ENV['EMAIL']",
-    password:             "ENV['PASSWORD']",
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD']
+}
 end
