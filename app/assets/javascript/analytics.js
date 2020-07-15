57ebb0b4a60af29d2a6a96dfce291c152f3eb793
Otto <ottoburney@gmail.com>
2020-07-15 10:09:36 +0100

    changed location of analytics.js

--- app/assets/javascript/analytics.js
+++ app/assets/javascript/analytics.js
@@ -7,6 +7,6 @@ const trackGoogleAnalytics = (event) => {
   gtag('config', 'GA_MEASUREMENT_ID', {
     'cookie_flags': 'max-age=7200;secure;samesite=none'
   })
-}
 
 document.addEventListener('turbolinks:load', trackGoogleAnalytics)
+
