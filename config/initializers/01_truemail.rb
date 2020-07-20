956ee2ba31edea8c3c9fd8112e5fecea48711ebc
Otto <ottoburney@gmail.com>
2020-07-20 11:02:58 +0100

    truemail now working- deletes emails that aren't in valid format

--- config/initializers/01_truemail.rb
+++ config/initializers/01_truemail.rb
@@ -2,6 +2,7 @@ require 'truemail'
 
 Truemail.configure do |config|
   config.verifier_email = 'verifier@example.com'
+  config.verifier_domain = 'somedomain.com'
   config.whitelisted_domains = ['white-domain.com', 'somedomain.com']
   config.blacklisted_domains = ['black-domain.com', 'somedomain.com']
   config.validation_type_for = { 'somedomain.com' => :mx }
@@ -11,8 +12,6 @@ all_subscribers = Subscriber.all
 p all_subscribers
 p "deleting invalid subscribers"
 all_subscribers.each do |s|
-  validation = Truemail.validate('s.email', with: :regex)
-  p s
-  p validation.result[0]
+  validation = Truemail.validate("#{s.email}", with: :regex)
   validation.result[0] ? s : s.delete
 end
