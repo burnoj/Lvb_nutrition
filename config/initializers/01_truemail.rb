49dd66e1dc278e548b7f2160e4c88f9856f5d39f
Otto <ottoburney@gmail.com>
2020-07-27 15:53:06 +0100

    reintroduced truemail, needed to add heroku config for SIB_API_KEY

--- config/initializers/01_truemail.rb
+++ config/initializers/01_truemail.rb
@@ -1,17 +1,17 @@
-# require 'truemail'
+require 'truemail'
 
-# Truemail.configure do |config|
-#   config.verifier_email = 'verifier@example.com'
-#   config.verifier_domain = 'somedomain.com'
-#   config.whitelisted_domains = ['white-domain.com', 'somedomain.com']
-#   config.blacklisted_domains = ['black-domain.com', 'somedomain.com']
-#   config.validation_type_for = { 'somedomain.com' => :mx }
-# end
+Truemail.configure do |config|
+  config.verifier_email = 'verifier@example.com'
+  config.verifier_domain = 'somedomain.com'
+  config.whitelisted_domains = ['white-domain.com', 'somedomain.com']
+  config.blacklisted_domains = ['black-domain.com', 'somedomain.com']
+  config.validation_type_for = { 'somedomain.com' => :mx }
+end
 
-# all_subscribers = Subscriber.all
-# p all_subscribers
-# p "deleting invalid subscribers"
-# all_subscribers.each do |s|
-#   validation = Truemail.validate("#{s.email}", with: :regex)
-#   validation.result[0] ? s : s.delete
-# end
+all_subscribers = Subscriber.all
+p all_subscribers
+p "deleting invalid subscribers"
+all_subscribers.each do |s|
+  validation = Truemail.validate("#{s.email}", with: :regex)
+  validation.result[0] ? s : s.delete
+end
